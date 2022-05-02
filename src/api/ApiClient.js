1f723dc367d226f0870fd5ecf81b586a9615b1c8
Leeran <leeran7@gmail.com>
2022-05-02 17:20:32 -0400

    added more functions into api

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -1,6 +1,9 @@
 
 class ApiClient {
-    
+    constructor(key, baseUrl, end) {
+        this.baseUrl = 'https://api.themoviedb.org/3';
+        
+    }
     getFullUrl(url) {
         return `${this.baseUrl}${url}${this.end}`;
     }
@@ -11,6 +14,29 @@ class ApiClient {
 
     getGenreList(){
         return this.get(`/genre/movie/list`);
+    }
+
+    getTvShow(id){
+        return this.get(`/tv/${id}`);
+    }
+
+    getTvShowSeason(id, seasonNum){
+        return this.get(`/tv/${id}/season/${seasonNum}`);
+    }
+
+    getTvShowEpisodes(id, seasonNum, episodeNum){
+        return this.get(`/tv/${id}/season/${seasonNum}/episode/${episodeNum}`);
+    }
+
+    getTrendingMovies(){
+        return this.get(`/trending/movie/week`);
+    }
+    
+    getTrendingTvShows(){
+        return this.get(`/trending/tv/week`);
+    }
+    getDiscoverMovies(){
+        return this.get(`/discover/movie`);
     }
 
 
