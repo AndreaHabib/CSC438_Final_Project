f96f78658eff3fb578a730763ac4392edce7faf3
Leeran <leeran7@gmail.com>
2022-05-02 17:31:17 -0400

    removed post/delete request

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -1,9 +1,11 @@
 
 class ApiClient {
+    
     constructor(key, baseUrl, end) {
         this.baseUrl = 'https://api.themoviedb.org/3';
         this.end = `?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
     }
+
     getFullUrl(url) {
         return `${this.baseUrl}${url}${this.end}`;
     }
@@ -35,12 +37,10 @@ class ApiClient {
     getTrendingTvShows(){
         return this.get(`/trending/tv/week`);
     }
+
     getDiscoverMovies(){
         return this.get(`/discover/movie`);
     }
-
-
-
 
     async get(url) {
         const fullUrl = this.getFullUrl(url);
@@ -48,38 +48,6 @@ class ApiClient {
         .then(response => response.json())
         .then(data => data.results)
         .catch(e => e.message)
-    }
-    
-    async post(url, data) {
-        const fullUrl = this.getFullUrl(url);
-        return await fetch(fullUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-        })
-        .then(response => response.json());
-    }
-    
-    async put(url, data) {
-        const fullUrl = this.getFullUrl(url);
-        return await fetch(fullUrl, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-        })
-        .then(response => response.json());
-    }
-    
-    async delete(url) {
-        const fullUrl = this.getFullUrl(url);
-        return await fetch(fullUrl, {
-        method: 'DELETE'
-        })
-        .then(response => response.json());
     }
 }
 export default ApiClient;
