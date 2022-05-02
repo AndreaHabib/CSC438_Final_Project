ef1767b025ba29741b8d1fc94de4e0687a9c6293
cwang5336 <cwang5336@gmail.com>
2022-05-02 14:09:48 -0400

    added list component and routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Signup from './components/signup/signup';
 import Home from './components/home/home';
 import Login from './components/login/login';
+import List from './components/list/list';
 // import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route exact path="/" element={<Login/>} />
         <Route exact path="/signup" element={<Signup/>} />
         <Route exact path="/home" element={<Home/>} />
-        <Route exact path="/" element={<Login/>} />
+        <Route exact path="/list" element={<List/>} />
       </Routes>
     </Router>
   );
