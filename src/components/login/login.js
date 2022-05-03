258fd9c96de3e3f6ddb3db14dc2f18affbcaef07
Leeran <leeran7@gmail.com>
2022-05-03 16:30:58 -0400

    getUserInfo

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,57 +1,80 @@
-
-import { useState, React } from 'react';
-import { signInWithEmailAndPassword, getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { Link } from "react-router-dom"
-import './login.css';
-
+import { useState, React, useEffect } from "react";
+import {
+  signInWithEmailAndPassword,
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { Link } from "react-router-dom";
+import "./login.css";
+import { loginInWithGoogle, getUserInfo } from "../../firebase-config";
 
 function Login() {
-    const [loginEmail, setLoginEmail] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
-    const [user, setUser] = useState({});
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState({});
 
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  // sample code from firebase-config.js
+  //   useEffect(() => {
+  //     getUserInfo("QWFhKYtjK6UuqsRFt5YwrbtbShh2").then((data) => {
+  //       console.log(data);
+  //     });
+  //   }, []);
 
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
 
-    
+    if (user) {
+      window.location.href = "/home";
+    }
+  };
 
+  return (
+    <div>
+      <h1>Login</h1>
 
-    const login = async () => {
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(event) => {
+          setLoginEmail(event.target.value);
+        }}
+      />
 
-        try {
-            const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-            console.log(user);
-        } catch (error) {
-            console.log(error.message);
-        }
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(event) => {
+          setLoginPassword(event.target.value);
+        }}
+      />
 
-        if (user) {
-            window.location.href = "/home";
-        }
+      <button onClick={login}>Login</button>
 
-    };
+      <button className="login-provider-button" onClick={loginInWithGoogle}>
+        <img
+          height="40px"
+          src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+        />
+        <span> Continue with Google</span>
+      </button>
 
-    return (
-        <div>
-
-            <h1>Login</h1>
-
-            <input type="email" placeholder="Email" onChange={(event) => { setLoginEmail(event.target.value) }} />
-
-            <input type="password" placeholder="Password" onChange={(event) => { setLoginPassword(event.target.value) }} />
-
-            <button onClick={login}>Login</button>
-
-            <button className="login-provider-button" onClick={signInWithGoogle}>
-                <img height="40px" src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" />
-                <span> Continue with Google</span>
-            </button>
-
-            <p>Need to SignUp? <Link to="/signup"> Click Here </Link></p>
-
-        </div>
-    );
+      <p>
+        Need to SignUp? <Link to="/signup"> Click Here </Link>
+      </p>
+    </div>
+  );
 }
 
 export default Login;
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,81 +1,93 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, serverTimestamp, collection, doc, setDoc, addDoc } from 'firebase/firestore/lite'
-import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword} from "firebase/auth";
+import {
+  getFirestore,
+  serverTimestamp,
+  collection,
+  doc,
+  setDoc,
+  addDoc,
+  getDocs,
+} from "firebase/firestore/lite";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
+// Cheng database
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBlvfpqekQfced27CTFDIapAya1hE6yUhY",
+//   authDomain: "mymovie-348723.firebaseapp.com",
+//   projectId: "mymovie-348723",
+//   storageBucket: "mymovie-348723.appspot.com",
+//   messagingSenderId: "1039244055888",
+//   appId: "1:1039244055888:web:197a7e939fc168efe80855",
+//   measurementId: "G-FT3N9JJZQ9",
+// };
 
+// Leeran database
 const firebaseConfig = {
-    apiKey: "AIzaSyBlvfpqekQfced27CTFDIapAya1hE6yUhY",
-    authDomain: "mymovie-348723.firebaseapp.com",
-    projectId: "mymovie-348723",
-    storageBucket: "mymovie-348723.appspot.com",
-    messagingSenderId: "1039244055888",
-    appId: "1:1039244055888:web:197a7e939fc168efe80855",
-    measurementId: "G-FT3N9JJZQ9"
+  apiKey: "AIzaSyDc4xgDSlBHf5pCCud310bnY5Jv5Fh0yZs",
+  authDomain: "mymovie-8d5f9.firebaseapp.com",
+  projectId: "mymovie-8d5f9",
+  storageBucket: "mymovie-8d5f9.appspot.com",
+  messagingSenderId: "1503165230",
+  appId: "1:1503165230:web:9c9d3e937accb8c21b58bf",
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export const auth = getAuth(app);
 
+export async function getUserInfo(uid) {
+  const users = collection(db, "users");
+  const user = await getDocs(users, {
+    where: ["uid", "==", uid],
+  });
+  return user.docs.map((doc) => doc.data())[0];
+}
+
 async function userlogin(email, password) {
-    try {
-        const user = await auth.signInWithEmailAndPassword(email, password);
-        console.log(user);
-    } catch (error) {
-        console.log(error.message);
-    }
-    if (user) {
-        return user;
-    }
+  try {
+    const user = await auth.signInWithEmailAndPassword(email, password);
+    return user;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function userregister(email, password) {
-    try {
-        const user = await auth.createUserWithEmailAndPassword(email, password);
-        console.log(user);
-    } catch (error) {
-        console.log(error.message);
-    }
-    if (user) {
-        return user;
-    }
+  try {
+    const user = await auth.createUserWithEmailAndPassword(email, password);
+    return user;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-async function loginInWithGoogle() {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
-            window.location.href = "/home";
-        }).catch((error) => {
-            const errorCode = error.code;
-            console.log(errorCode);
-            const errorMessage = error.message;
-            console.log(errorMessage);
-            const email = error.email;
-            console.log(email);
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            console.log(credential);
-
-        });
-
+export async function loginInWithGoogle() {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      window.location.href = "/home";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      console.log(errorCode);
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      const email = error.email;
+      console.log(email);
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log(credential);
+    });
 }
 
 async function signOut() {
-    await auth.signOut();
+  await auth.signOut();
 }
-
-
-
-
-
-
-export function getFavoriteMovies(uid) {
-    return collection('users').doc(uid).collection('favoriteMovies').get();
-}
-
-
-
-
