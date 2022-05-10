b6eb2866716688f9438cae81129e5808a56946ad
cwang5336 <cwang5336@gmail.com>
2022-05-10 13:55:37 -0400

    trailers page where users can search up their movies by name and watch trailers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/signup/Signup";
-import Home from "./components/home/Home";
-import Login from "./components/login/Login";
+import Home from "./components/home/home";
+import Login from "./components/login/login";
 import Hero from "./components/hero/Hero";
 import Movie from "./components/movie/Movie";
 import Favorites from "./components/favorites/Favorites";
 import Show from "./components/show/Show";
+import Trailer from "./components/trailer/trailer";
 // import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route exact path="/favorites" element={<Favorites />} />
         <Route exact path="/movie/:id" element={<Movie />} />
         <Route exact path="/show/:id" element={<Show />} />
+        <Route exact path="/trailer" element={<Trailer />} />
         <Route exact path="/" element={<Hero />} />
       </Routes>
     </Router>
