586eaf0148812ff1ffddbdbe74bcb2a6320697e8
Huilin <huilin.zou@cix.csi.cuny.edu>
2022-05-10 16:12:18 -0400

    chores

---
+++ src/components/favorites/Favorites.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { getUserInfo } from "../../firebase-config";
+import { Typography, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import NavBar from "../navbar/NavBar";
+import List from "../List";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../../Styles";
+
+const Favorites = () => {
+  const [userInfo, setUserInfo] = useState({
+    favoriteMovies: [],
+    favoriteTvShows: [],
+  });
+
+  useEffect(() => {
+    getUserInfo().then(async (info) => {
+      const { favoriteMovies, favoriteTvShows } = info;
+      favoriteMovies.map(async (movie) => {
+        await fetch(
+          `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            setUserInfo((prevState) => ({
+              ...prevState,
+              favoriteMovies: [...prevState.favoriteMovies, data],
+            }));
+          });
+      });
+      favoriteTvShows.map(async (show) => {
+        await fetch(
+          `https://api.themoviedb.org/3/tv/${show}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            setUserInfo((prevState) => ({
+              ...prevState,
+              favoriteTvShows: [...prevState.favoriteTvShows, data],
+            }));
+          });
+      });
+    });
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <NavBar />
+      <h1>Favorites</h1>
+      <List
+        list={userInfo.favoriteMovies}
+        title="Favorite Movies"
+        type="movie"
+      />
+      <List
+        list={userInfo.favoriteTvShows}
+        title="Favorite Tv Shows"
+        type="show"
+      />
+    </ThemeProvider>
+  );
+};
+
+export default Favorites;
