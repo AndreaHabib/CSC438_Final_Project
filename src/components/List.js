3463d4c220200e15ee16eef24e70a43b6c82dff4
Leeran <leeran7@gmail.com>
2022-05-10 13:18:44 -0400

    turned lists into a component

---
+++ src/components/List.js
@@ -0,0 +1,59 @@
+import { Fragment } from "react";
+import { Box, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+const List = ({ list, title, type }) => {
+  const navigate = useNavigate();
+  return (
+    <Fragment>
+      <Typography
+        textAlign="center"
+        variant="h6"
+        component="div"
+        sx={{
+          width: "30%",
+          borderRadius: "20px",
+          p: 1,
+          margin: "0 auto",
+          backgroundColor: "primary.light",
+          color: "white",
+        }}
+      >
+        {title}
+      </Typography>
+      <Box sx={{ width: "100%", display: "flex", overflowX: "scroll" }}>
+        {list.map((item) => (
+          <Box
+            key={item.id}
+            sx={{
+              //   width: "100%",
+              m: 2,
+              p: 1,
+              borderRadius: "20px",
+              backgroundColor: "white",
+              "&:hover": {
+                cursor: "pointer",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+              },
+            }}
+            onClick={() => {
+              navigate(`/${type}/${item.id}`);
+            }}
+          >
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+              alt={item.title}
+              style={{
+                borderRadius: "20px",
+                width: "200px",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+          </Box>
+        ))}
+      </Box>
+    </Fragment>
+  );
+};
+
+export default List;
--- src/components/favorites/Favorites.js
+++ src/components/favorites/Favorites.js
@@ -3,7 +3,7 @@ import { getUserInfo } from "../../firebase-config";
 import { Typography, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../navbar/NavBar";
-
+import List from "../List";
 const Favorites = () => {
   const [userInfo, setUserInfo] = useState({
     favoriteMovies: [],
@@ -39,106 +39,21 @@ const Favorites = () => {
       });
     });
   }, []);
-  const navigate = useNavigate();
 
   return (
     <div>
       <NavBar />
       <h1>Favorites</h1>
-      <Typography
-        textAlign="center"
-        variant="h6"
-        component="div"
-        sx={{
-          width: "30%",
-          borderRadius: "20px",
-          p: 1,
-          margin: "0 auto",
-          backgroundColor: "primary.light",
-          color: "white",
-        }}
-      >
-        Favorite Movies
-      </Typography>
-      <Box sx={{ width: "100%", display: "flex", overflowX: "scroll" }}>
-        {userInfo.favoriteMovies.map((movie) => (
-          <Box
-            key={movie.id}
-            sx={{
-              width: "100%",
-              m: 2,
-              p: 1,
-              borderRadius: "20px",
-              backgroundColor: "white",
-              "&:hover": {
-                cursor: "pointer",
-                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
-              },
-            }}
-            onClick={() => {
-              navigate(`/movie/${movie.id}`);
-            }}
-          >
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt={movie.title}
-              style={{
-                borderRadius: "20px",
-                width: "200px",
-                height: "100%",
-                objectFit: "cover",
-              }}
-            />
-          </Box>
-        ))}
-      </Box>
-      <Typography
-        textAlign="center"
-        variant="h6"
-        component="div"
-        sx={{
-          width: "30%",
-          borderRadius: "20px",
-          p: 1,
-          margin: "0 auto",
-          backgroundColor: "primary.light",
-          color: "white",
-        }}
-      >
-        Favorite TV Shows
-      </Typography>
-      <Box sx={{ width: "100%", display: "flex", overflowX: "scroll" }}>
-        {userInfo.favoriteTvShows.map((show) => (
-          <Box
-            key={show.id}
-            sx={{
-              width: "100%",
-              m: 2,
-              p: 1,
-              borderRadius: "20px",
-              backgroundColor: "white",
-              "&:hover": {
-                cursor: "pointer",
-                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
-              },
-            }}
-            onClick={() => {
-              navigate(`/show/${show.id}`);
-            }}
-          >
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${show.poster_path}`}
-              alt={show.title}
-              style={{
-                borderRadius: "20px",
-                width: "200px",
-                height: "100%",
-                objectFit: "cover",
-              }}
-            />
-          </Box>
-        ))}
-      </Box>
+      <List
+        list={userInfo.favoriteMovies}
+        title="Favorite Movies"
+        type="movie"
+      />
+      <List
+        list={userInfo.favoriteTvShows}
+        title="Favorite Tv Shows"
+        type="tv"
+      />
     </div>
   );
 };
