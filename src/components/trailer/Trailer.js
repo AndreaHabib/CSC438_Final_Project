88e6a5525be25481503bfe6b48fa17fd9948c05a
Leeran <leeran7@gmail.com>
2022-05-11 13:31:20 -0400

    trimmed down trailer

--- src/components/trailer/Trailer.js
+++ src/components/trailer/Trailer.js
@@ -12,33 +12,18 @@ export default function Trailer() {
 
   const getTrailer = async (e) => {
     e.preventDefault();
-    let apiURL = `https://api.themoviedb.org/3/search/movie?api_key=bacc58af01e82926a93e6c84b55f9c5f&query=${search}&page=1`;
-
+    const apiURL = `https://api.themoviedb.org/3/search/movie?api_key=bacc58af01e82926a93e6c84b55f9c5f&query=${search}&page=1`;
     const response = await fetch(apiURL);
     const json = await response.json();
-
     setTitle(json.results[0].original_title);
-
-    console.log(title);
-
     for (let i = 0; i < json.results.length; i++) {
       array[i] = json.results[i].id;
     }
-
     let movieId = array[0];
-
-    let apiURL2 = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=bacc58af01e82926a93e6c84b55f9c5f&language=en-US`;
-
+    const apiURL2 = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=bacc58af01e82926a93e6c84b55f9c5f&language=en-US`;
     const response2 = await fetch(apiURL2);
     const json2 = await response2.json();
-    console.log(json2);
-
     setkey(json2.results[0].key.toString());
-
-    console.log(key);
-
-    document.querySelector(".video").style.display = "block";
-    document.querySelector(".cont-trailer").style.display = "block";
   };
 
   return (
@@ -46,15 +31,6 @@ export default function Trailer() {
       <NavBar />
 
       <div>
-        {/* <form onSubmit={getTrailer}>
-          <input
-            placeholder="Specific Movie Name"
-            type="text"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button type="submit"> Search </button>
-        </form> */}
         <FormGroup className="form">
           <FormControl variant="standard" sx={{ width: "100%", m: 0.5 }}>
             <TextField
@@ -75,14 +51,12 @@ export default function Trailer() {
         </FormGroup>
 
         {key && (
-          <div className="video">
-            <iframe
-              width="100%"
-              height="100%"
-              title="trailer"
-              src={`https://www.youtube.com/embed/${key}`}
-            ></iframe>
-          </div>
+          <iframe
+            width="100%"
+            height="100%"
+            title="trailer"
+            src={`https://www.youtube.com/embed/${key}`}
+          ></iframe>
         )}
       </div>
     </Fragment>
